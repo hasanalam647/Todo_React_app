2c29f91f4ed72777c57b25e1435ca776fe903d4c
Muhammad Azeem <azeemkhanpathanpk@gmail.com>
2023-02-02 15:49:01 +0500

    Added functionality on Mark Done Button Added Local Storage to save data

--- src/Components/Todos-app/todos.js
+++ src/Components/Todos-app/todos.js
@@ -1,42 +1,83 @@
-import React, { useState } from "react";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Form, InputGroup } from "react-bootstrap";
 import TodosItem from "../items/Todos-Item";
 import "./todos.css";
+
+const key = "todoItems";
 
 const TodosList = () => {
   const [value, setValue] = useState("");
   const [todoItem, setTodoItem] = useState([]);
 
+  useEffect(() => {
+    let todoItems = JSON.parse(localStorage.getItem(key) || "[]");
+    if (todoItems && todoItems.length) {
+      setTodoItem(todoItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(todoItem));
+  }, [todoItem]);
+
   const handleChange = (event) => {
     setValue(event.target.value);
   };
-  const handleAdd = () => {
-    setTodoItem([...todoItem, { value, id: Date.now() }]);
+  const handleAdd = (event) => {
+    event.preventDefault();
+
+    if (value.length <= 0 || value.trim() === "") {
+      alert("Please Enter Valid Task");
+    } else {
+      setTodoItem([...todoItem, { value, id: Date.now(), isDone: false }]);
+    }
+
     setValue("");
   };
-  const handleDelete = (id) => {
+  const handleDone = (id) => {
     const todosItemIndex = todoItem.findIndex((value) => value.id === id);
-    if(todosItemIndex >= 0){
-    todoItem.splice(todosItemIndex, 1);
-    setTodoItem([...todoItem]);
-  }
+    let todoDone = todoItem[todosItemIndex];
+    todoDone.isDone = !todoDone.isDone;
+    console.log("id", id);
+    console.log("todosItemIndex", todosItemIndex);
+    console.log("todoDone", todoDone);
+
+    if (todosItemIndex >= 0) {
+      todoItem.splice(todosItemIndex, 1, todoDone);
+      setTodoItem([...todoItem]);
+    }
   };
 
-  const  handleSave=(id, newvalue) => {
+  const handleDelete = (id) => {
     const todosItemIndex = todoItem.findIndex((value) => value.id === id);
-    if(todosItemIndex >= 0) {
-      todoItem.splice(todosItemIndex, 1, {value: newvalue, id});
-    setTodoItem([...todoItem])
+    if (todosItemIndex >= 0) {
+      todoItem.splice(todosItemIndex, 1);
+      setTodoItem([...todoItem]);
     }
-    
-    };
+  };
+
+  const handleSave = (id, newvalue, event) => {
+    event.preventDefault();
+
+    const todosItemIndex = todoItem.findIndex((value) => value.id === id);
+    if (newvalue <= 0 || newvalue.trim() === "") {
+      alert("PLease enter Valid Task ");
+    } else {
+      if (todosItemIndex >= 0) {
+        todoItem.splice(todosItemIndex, 1, { value: newvalue, id });
+      }
+      setTodoItem([...todoItem]);
+    }
+  };
 
   return (
     <>
       <Container>
         <h3 className="title">My Todos App</h3>
 
-        <InputGroup className="mb-3">
+        <InputGroup as="form" className="mb-3" onSubmit={handleAdd}>
           <Form.Control
             placeholder="Please Enter Task"
             aria-label="Recipient's username"
@@ -44,22 +85,19 @@ const TodosList = () => {
             value={value}
             onChange={handleChange}
           />
-          <Button
-            variant="outline-secondary"
-            id="button-addon2"
-            onClick={handleAdd}
-          >
-            Add
+          <Button variant="success" id="button-addon2" type="submit">
+            <FontAwesomeIcon icon={faPlus} />
           </Button>
         </InputGroup>
         {todoItem.map((value) => (
           <TodosItem
             key={value.id}
             id={value.id}
+            isDone={value.isDone}
             todosItem={value.value}
             handleDelete={handleDelete}
             handleSave={handleSave}
-            
+            handleDone={handleDone}
           />
         ))}
       </Container>
