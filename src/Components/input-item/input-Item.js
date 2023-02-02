827921826178b5068f36a4705f4ed270965a35aa
Muhammad Azeem <azeemkhanpathanpk@gmail.com>
2023-02-02 15:50:19 +0500

    Added font awesome icons to buttons

--- src/Components/input-item/input-Item.js
+++ src/Components/input-item/input-Item.js
@@ -1,3 +1,5 @@
+import { faFloppyDisk, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
@@ -8,9 +10,16 @@ const InputItems = (props) => {
   };
   return (
     <>
-      <Row>
+      <Row
+        as="form"
+        onSubmit={(event) => {
+          props.handleSave(props.id, newvalue, event);
+          props.handleEdit();
+        }}
+      >
         <Col lg={10} sm={8}>
           <Form.Control
+            className="btn-edit"
             placeholder="Please Enter Task"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
@@ -20,17 +29,21 @@ const InputItems = (props) => {
         </Col>
         <Col lg={2} sm={4}>
           <Button
-            variant="primary"
+            variant="success"
             size="md"
-            onClick={() => {
-              props.handleSave(props.id, newvalue);
-              props.handleEdit();
-            }}
+            className="btn-edit"
+            type="submit"
+            
           >
-            save
+            <FontAwesomeIcon icon={faFloppyDisk} />
           </Button>
-          <Button variant="primary" size="md" onClick={props.handleEdit}>
-            cancel
+          <Button
+            variant="danger"
+            size="md"
+            className="btn-edit"
+            onClick={props.handleEdit}
+          >
+            <FontAwesomeIcon icon={faX} />
           </Button>
         </Col>
       </Row>
--- src/Components/items/Todos-Item.js
+++ src/Components/items/Todos-Item.js
@@ -1,42 +1,60 @@
-import React, { useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import {
+  faCheckToSlot,
+  faPenToSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {useState } from "react";
+import { Button, Col, Container, Row} from "react-bootstrap";
 import InputItems from "../input-item/input-Item";
 
 const TodosItem = (props) => {
   const [isEditable, setIsEditable] = useState(false);
+ 
 
   const handleEdit = () => {
-    setIsEditable(!isEditable)
-  }
-
+    setIsEditable(!isEditable);
+  };
 
   return (
-      <Container>
-        {isEditable?
+    <Container className="conainter-1">
+      {isEditable ? (
         <InputItems {...props} handleEdit={handleEdit} />
-        :
+      ) : (
         <Row>
-          <Col lg={10} sm={8}>
+          <Col lg={10} sm={8} className={`col-1 ${props.isDone ? "done" : ""}`}>
             {props.todosItem}
           </Col>
-          <Col lg={2} sm={4}>
+          <Col lg={2} sm={4} className="col-1">
+            <Button
+              variant="success"
+              size="md"
+              className="btn-edit"
+              onClick={() => props.handleDone(props.id)}
+            >
+              <FontAwesomeIcon icon={faCheckToSlot} />
+            </Button>
+
             <Button
               variant="primary"
               size="md"
+              className="btn-edit"
               onClick={handleEdit}
             >
-              edit
+              <FontAwesomeIcon icon={faPenToSquare} />
             </Button>
             <Button
-              variant="primary"
+              variant="danger"
               size="md"
+              className="btn-edit"
               onClick={() => props.handleDelete(props.id)}
             >
-              delete
+              <FontAwesomeIcon icon={faTrash} />
             </Button>
           </Col>
-        </Row>}
-      </Container>
+        </Row>
+      )}
+    </Container>
   );
 };
 
